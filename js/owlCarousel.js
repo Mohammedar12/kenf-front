b1b7cee37870796988289d4b8d53569797457087
unknown <alikkar87@gmail.com>
2022-09-26 19:55:49 +0300

    s

--- js/owlCarousel.js
+++ js/owlCarousel.js
@@ -1,6 +1,21 @@
-// $(document).ready(function () {
-//   $(".owl-carousel").owlCarousel();
-// });
+$(".banners-main").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navSpeed: 800,
+  dotsSpeed: 800,
+  dragEndSpeed: 800,
+  responsive: {
+    0: {
+      items: 1,
+    },
+  },
+});
+
+// change nav icon 
+
+document.querySelector(".banners-main .owl-prev").innerHTML = `<i class="fa-solid fa-angle-left fa-2x"></i>`
+document.querySelector(".banners-main .owl-next").innerHTML = `<i class="fa-solid fa-angle-right fa-2x"></i>`
 
 $(".exclusive-carousel").owlCarousel({
   center: true,
@@ -8,6 +23,7 @@ $(".exclusive-carousel").owlCarousel({
   margin: 10,
   autoplay: true,
   autoplaySpeed: 1000,
+  dragEndSpeed: 800,
   nav: false,
   dots: false,
   responsive: {
@@ -31,6 +47,7 @@ $(".suggested-products").owlCarousel({
   margin: 10,
   autoplay: true,
   autoplaySpeed: 1000,
+  dragEndSpeed:700,
   nav: false,
   dots: false,
   responsive: {
